ec6c537e70d4060b1a3acd59c4b1be42a9d5a7f2
stuartw <stuwilson@gmail.com>
2024-12-31 11:41:46 +0000

    added groups for drawable and updateable

--- main.py
+++ main.py
@@ -7,16 +7,17 @@ from player import Player
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    my_clock = pygame.time.Clock()
-    dt = 0
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
 
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
-
+    my_clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -24,10 +25,14 @@ def main():
         # limit the fps to 60
         dt = my_clock.tick(60) / 1000
 
-        player.update(dt)
+        for obj in updateable:
+            obj.update(dt)
 
         screen.fill("black")
-        player.draw(screen)        
+
+        for obj in drawable:
+            obj.draw(screen) 
+
         pygame.display.flip()
         
         
